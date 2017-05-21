2f7035d7b9647072927abe5051da350be86079a6
Jarred Capellman <jcapellman@hotmail.com>
2017-05-21 12:00:14 -0500

    Added: Initial Commit

---
+++ src/GalacticTrader/GalacticTrader/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using GalacticTrader.ViewModels;
+
+using Xamarin.Forms;
+
+namespace GalacticTrader.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+	}
+}
